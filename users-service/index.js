9de27f9325e6b5f29b1d29618485eabe3ec6fb53
odedi <odedisa@gmail.com>
2026-08-19 22:18:11 +0300

    Complete users service endpoints

--- users-service/index.js
+++ users-service/index.js
@@ -66,7 +66,7 @@ app.post('/api/add', function (req, res) {
             return res.status(201).json(createdUser);
         })
         .catch(function(error) {
-            if (error === errors.USER_ALREADY_EXISTS){ // לחזור לנקודה הזאת ולשנות!!!! סשן 3 ERROR HANDLING
+            if (error.id === errors.USER_ALREADY_EXISTS.id){ // Handles the duplicate user error separately from unexcepted server errors
                 return res.status(409).json(errors.USER_ALREADY_EXISTS);
                 //HTTP 409 Conflict (The request could not be processed because of a conflict with the current state)
             }
@@ -92,11 +92,26 @@ app.get('/api/users/:id', function (req, res) {
     if (Number.isNaN(requestedUserId)) { // Validates that the user ID in the URL is a valid number
         return res.status(400).json(errors.INVALID_USER_INPUT);
     }
-    User.findOne({ id: requestedUserId }) // Finds the user by the application-specific ID
-        .then(function(user){
-            if(!user) {
-                return res.status(404).json(errors.USER_NOT_FOUND);
-                //HTTP 404 The server returns an error when no user matches the requested ID
-            }
-        });
+        User.findOne({ id: requestedUserId }) // Finds the user by the application-specific ID
+            .then(function(user) {
+                if (!user) {
+                    return res.status(404).json(errors.USER_NOT_FOUND);
+                    //HTTP 404 The server returns an error when no user matches the requested ID
+                }
+                return fetch(`http://localhost:3001/api/total/${requestedUserId}`) // Sends an HTTP GET request from the Users Service to the Costs Service
+                    .then(function(response) {
+                        return response.json(); // Parses the response body from JSON into a JavaScript object
+                    })
+                    .then(function (costData) {
+                        // CostData now contains the parsed data returned by the Costs Service
+                        return res.status(200).json({
+                            first_name: user.first_name,
+                            last_name: user.last_name,
+                            id: user.id,
+                            total: costData.total
+                        });
+                    });
+            }).catch(function(error){
+                return res.status(500).json(errors.INTERNAL_SERVER_ERROR);
+            });
 });
